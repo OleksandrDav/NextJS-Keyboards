87e13376815e09d561fbca6f57814d2bcdb49eae
OleksandrDav <o.sec.dav@gmail.com>
2025-03-12 16:34:39 +0100

    Header layout Add shared components: Header, Container, and Title; update layout and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Container } from "@/components/shared";
+import { Title } from "@/components/shared/Title";
+
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Button variant="outline">Click me</Button>
-    </div>
-  );
+  return <>
+  
+  <Container className="mt-5">
+    <Title text='All Keyboards' size='lg' className="font-extrabold"/>
+  </Container>
+  
+  </>;
 }
